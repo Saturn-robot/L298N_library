a4f75d51cf09aa4f19aa6eaa32d437a107f7b251
Chaoyang Liu <chaoyanglius@gmail.com>
2017-03-08 16:35:29 +0800

    first commit

---
+++ driver/command.h
@@ -0,0 +1,14 @@
+
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#define CLEFT       'l'
+#define CRIGHT      'r'
+
+#define ALLWHEELS	'A'
+#define STOPWHEEL       's'
+#define STOPWHEELS      'S'
+#define FORWARDS        true
+#define BACKWARDS       false
+
+#endif
