8cf74dbf46af77119da1dca7f85d670254110d7f
Chaoyang Liu <chaoyanglius@gmail.com>
2017-03-13 08:58:39 +0800

    improve the copyright

--- driver/driver.ino
+++ driver/driver.ino
@@ -1,15 +1,26 @@
 /***********************************************************************
- *  DUAL FULL-BRIDGE L298N driver
- *
  * Author: Chaoyang Liu
  * E-main: chaoyanglius@outlook.com
  *
- * Inspired and modeled after the Pirobot driver by Patrick Goebel
- *
  * Software License Agreement (GPL License)
+ * DUAL FULL-BRIDGE L298N driver, Inspired and modeled after the
+ * Pirobot driver by Patrick Goebel
  * Copyright (c) 2017, Chaoyang Liu
  *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ************************************************************************/
+
 #include "motor_driver.h"
 #include "command.h"
 
